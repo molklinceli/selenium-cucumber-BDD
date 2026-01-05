6e1557492f1efaccb3ba07c866501cb9d491c37f
ines sakesli <sakesliinesmlc@gmail.com>
2026-01-05 23:18:08 +0100

    Login Feature

--- src/test/java/com/e2eTest/automation/RunWebSuiteTest.java
+++ src/test/java/com/e2eTest/automation/RunWebSuiteTest.java
@@ -1,5 +1,22 @@
 package com.e2eTest.automation;
 
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import static io.cucumber.junit.CucumberOptions.SnippetType.CAMELCASE;
+
+import org.junit.runner.RunWith; 
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+		features = {"src/spec/features"},
+		plugin = {"pretty","html: target/report/cucumber-report.html", "json: target/report/cucumber.json"},
+		//glue = {"src/test/java/com/e2eTest/automation/step_definitions"},
+		tags = ("@login"),
+		monochrome = true,
+		snippets = CAMELCASE
+		)
+
+
 public class RunWebSuiteTest {
 
 }
---
+++ src/test/java/com/e2eTest/automation/page_objects/LoginPage.java
@@ -0,0 +1,55 @@
+package com.e2eTest.automation.page_objects;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.How;
+
+import com.e2eTest.automation.utils.BasePage;
+import com.e2eTest.automation.utils.Setup;
+
+public class LoginPage extends BasePage {
+    
+	/*
+     * Retreive Web element
+     */
+	 @FindBy(how = How.CSS, using = ".ico-login")
+	 private static WebElement loginBarMenu;
+	
+	 @FindBy(how = How.XPATH, using = "//input[@id='Email']")
+	 private static WebElement emailField;
+	 
+	 @FindBy(how = How.XPATH, using = "//input[@id='Password']")
+	 private static WebElement passwordField;
+	 
+	 
+	 @FindBy(how = How.XPATH, using = "//input[@value='Log in']")
+	 private static WebElement loginButton;
+	 
+	 @FindBy(how = How.XPATH, using = "//a[normalize-space()='ziedhannachi0@gmail.com']")
+	 private static WebElement homePage;
+	 
+	 public LoginPage() {
+		super(Setup.getDriver());
+	}
+	
+	public static WebElement getLoginBarMenu() {
+		return loginBarMenu;
+	}
+	
+	public static WebElement getEmailField() {
+		return emailField;
+	}
+	
+	public static WebElement getPasswordField() {
+		return passwordField;
+	}
+	
+	public static WebElement getLoginButton() {
+		return loginButton;
+	}
+	
+	public static WebElement getHomePage() {
+		return homePage;
+	}
+	
+}
